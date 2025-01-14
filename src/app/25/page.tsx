40d2d33319a3d7ad035618b3726e8d3393e80c0f
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-01-14 18:32:22 +0530

    25 page added

---
+++ src/app/25/page.tsx
@@ -0,0 +1,13 @@
+import CalEmbed from '@/src/components/cal'
+import CalBooking from '@/src/components/cal-booking'
+import React from 'react'
+
+function Calpage() {
+  return (
+    <div className='py-24'>
+        <CalEmbed/>
+    </div>
+  )
+}
+
+export default Calpage
